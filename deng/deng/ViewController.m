07b798c299cd7a8d5ef3eaa1cd4c5cf2224e708f
yuanhaohua <yuanhaohua123@qq.com>
2016-02-29 20:17:32 +0800

    阳光

---
+++ deng/deng/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  deng
+//
+//  Created by yuan on 16/2/29.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"521");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
